4008a8de65613927fc32ef8ee1470d0f9c34467f
umairtahiri <umairtahiri9212@gmail.com>
2021-03-21 20:17:16 +0500

    updates

--- src/components/location-finder/index.js
+++ src/components/location-finder/index.js
@@ -1,21 +1,80 @@
-import React, { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import { OpenStreetMapProvider } from "leaflet-geosearch";
+import autocomplete from "autocompleter";
+
+import Loader from "../../image/Spinner-3.gif";
+
 import LeafMap from "../map";
 
 import "./styles.scss";
 
 const LocationFinder = () => {
-  const [location, setLocation] = useState("");
+  const provider = new OpenStreetMapProvider({
+    params: {
+      countrycodes: "IT",
+    },
+  });
+  const [startSearch, setStartSearch] = useState(false);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [placeForDetails, setPlaceForDetails] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getResults = async (str) => {
+    setStartSearch(false);
+    setLoading(true);
+    const places = await provider.search({ query: str });
+    const formattedPlaces = places.map((p) => ({
+      label: p?.label,
+      value: JSON.stringify(p),
+    }));
+    places && setLoading(false);
+    return formattedPlaces;
+  };
+
+  useEffect(() => {
+    var input = document.getElementById("places-input");
+
+    autocomplete({
+      input: input,
+      fetch: async (text, update) => {
+        const places = (await getResults(text)) || [];
+        update(places);
+      },
+      onSelect: (place) => {
+        const info = place && JSON.parse(place.value);
+        setSelectedPlace(place);
+        setPlaceForDetails(info);
+        input.value = place.label;
+      },
+    });
+  }, [startSearch]);
+
   return (
     <div className="location-finder-container">
       <div className="input-container">
-        <input
-          className="input"
-          onChange={(e) => setLocation(e.target.value)}
-          value={location}
-          placeholder="Search area"
-        />
+        <input id="places-input" className="input" placeholder="Search area" />
+        <div className="loader-container">
+          {loading && <img className="loader" src={Loader} alt="" />}
+        </div>
       </div>
-      <LeafMap />
+      <LeafMap place={selectedPlace} />
+      {placeForDetails && (
+        <section className="area-details-container">
+          <div className="row">
+            <div className="title">Place Name</div>
+            <div className="description">{placeForDetails.label}</div>
+          </div>
+          <div className="row">
+            <div className="title">Coordinates</div>
+            <div className="description">{`lat: ${placeForDetails.raw.lat}, lon: ${placeForDetails.raw.lon}`}</div>
+          </div>
+          <div className="row">
+            <div className="title">Place Type</div>
+            <div className="description">{placeForDetails.raw.type}</div>
+          </div>
+        </section>
+      )}
     </div>
   );
 };
--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,25 +1,72 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useMemo, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+import L from "leaflet";
+
 import "./styles.scss";
 
-const LeafMap = () => {
-  return (
-    <div className="map-size">
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const LeafMap = (props) => {
+  const { place } = props;
+  const [map, setMap] = useState({});
+
+  console.log({ place });
+
+  const getPosition = () => {
+    if (place) {
+      const selectedPlace = JSON.parse(place?.value) || {};
+      const lat = selectedPlace?.y || selectedPlace?.lat;
+      const lon = selectedPlace?.x || selectedPlace?.lon;
+      return [lat, lon];
+    } else return [41.902782, 12.496366];
+  };
+
+  const changePos = () => {
+    const position = getPosition();
+    if (map) map.flyTo && map.flyTo(position);
+  };
+
+  useEffect(() => {
+    changePos();
+  }, [place]);
+
+  const renderMap = useMemo(() => {
+    const position = getPosition();
+    return (
       <MapContainer
         className="markercluster-map"
-        center={[51.0, 19.0]}
+        center={position}
         zoom={12}
         maxZoom={18}
+        scrollWheelZoom={false}
+        whenCreated={(map) => setMap(map)}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        <Marker
+          position={position}
+          // icon={new L.Icon.Default()}
+        >
+          <Popup>{place?.label || "Rome"}</Popup>
+        </Marker>
       </MapContainer>
-    </div>
-  );
+    );
+  }, [place]);
+
+  return <div className="map-size">{renderMap}</div>;
 };
 
 export default LeafMap;
